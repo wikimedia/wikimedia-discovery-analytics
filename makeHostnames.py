ac42d7209cf6c86b9a005bd324121b5d35e39d67
Stanislav Malyshev <smalyshev@gmail.com>
2015-10-23 16:10:54 -0700

    Data loader for sending hive data to ES
    Run with:
    spark-submit --master yarn transferToES.py -s /tmp/source.parquet -u http://my.elastic/
    Bug: T113440
    Change-Id: Id4df5bffca445b396c875329bce005327110ddab

---
+++ makeHostnames.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+import subprocess
+import re
+import json
+import sys
+import datetime
+
+# This script produces the JSON map from hostnames - as stored in hive - to wiki dbnames
+
+# Get the list of hostnames
+now = datetime.datetime.now()
+mon = now.month - 1
+year = now.year
+if mon == 0:
+    mon = 12
+    year -= 1
+# This uses last months' data, since this month may be just beginning and not have good data yet
+hostnames = subprocess.check_output(["hive", "-S", "-e",
+    'select distinct project from wmf.discovery_popularity_score \
+        where year=%d AND month=%d;' % (year, mon)]).split("\n")
+names = {}
+# The source file should be updated from:
+# https://raw.githubusercontent.com/wikimedia/operations-mediawiki-config/master/dblists/all.dblist
+dblist = set(open("all.dblist").read().split())
+# The following abomination comes from:
+# https://github.com/wikimedia/operations-mediawiki-config/blob/master/multiversion/MWMultiVersion.php#L134
+# And should be updated when that changes.
+# No, I have no idea how to ensure it's up to date. That's why one shouldn't hardcode such things.
+staticMap = {
+    'wikimediafoundation': 'foundation',
+    'mediawiki': 'mediawiki',
+    'wikidata': 'wikidata',
+    'wikisource': 'sources',
+    'be-tarask.wikipedia': 'be_x_old',
+}
+chapters = set(['ar', 'bd', 'be', 'br', 'ca', 'cn', 'co', 'dk', 'et', 'fi', 'il', 'mk', 'mx', 'nl',
+    'noboard-chapters', 'no', 'nyc', 'nz', 'pa-us', 'pl', 'rs', 'ru', 'se', 'tr', 'ua', 'uk', 've'])
+hostmatch = re.compile('^(.*)\.([a-z]+)$')
+for hostname in hostnames:
+    if hostname == 'project' or len(hostname) == 0:
+        # skip this hive artifact
+        continue
+    site = "wikipedia"
+    if hostname in staticMap:
+        lang = staticMap[hostname]
+    else:
+        m = hostmatch.match(hostname)
+        if not m:
+            print("BAD HOSTNAME: " + hostname, file=sys.stderr)
+            continue
+        lang = m.group(1)
+        if m.group(2) != 'wikimedia' or lang in chapters:
+            site = m.group(2)
+    if site == 'wikipedia':
+        site = 'wiki'
+
+    dbname = (lang + site).replace("-", "_")
+    if dbname not in dblist:
+        print("UNKNOWN DBNAME: " + dbname, file=sys.stderr)
+        continue
+    names[hostname] = dbname
+
+print(json.dumps(names, sort_keys=True, indent=1))
---
+++ transferToES.py
@@ -0,0 +1,133 @@
+# A very primitive script to transfer from hive data files
+# to ElasticSearch HTTP endpoint
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+from optparse import OptionParser
+import requests
+import json
+import logging
+
+oparser = OptionParser()
+oparser.add_option("-s", "--source", dest="source", help="source for the data", metavar="SOURCE")
+oparser.add_option("-u", "--url", dest="url", help="URL to send the data to", metavar="URL")
+oparser.add_option("-m", "--hostmap", dest="hostmap", help="Hostnames map in JSON",
+    metavar="FILE", default="hostmap.json")
+oparser.add_option("-b", "--batch", dest="batch", help="Items per batch to load into ES", metavar="NUM", default="10")
+
+(options, args) = oparser.parse_args()
+
+ITEMS_PER_BATCH = int(options.batch)
+SOURCE = options.source
+TARGET = options.url
+hostMap = json.load(open(options.hostmap))
+
+print "Transferring from %s to %s" % (SOURCE, TARGET)
+
+if __name__ == "__main__":
+    sc = SparkContext(appName="Send To ES")
+    sqlContext = SQLContext(sc)
+    broardcastMap = sc.broadcast(hostMap)
+    documentCounter = sc.accumulator(0)
+    updateCounter = sc.accumulator(0)
+    errorCounter = sc.accumulator(0)
+    failedDocumentCounter = sc.accumulator(0)
+
+    def documentData(document):
+        """
+        Create textual representation of the document data for one document
+        """
+        updateData = {"update": {"_id": document.page_id}}
+        updateDoc = {"doc": {"score": document.score}}
+        return json.dumps(updateData) + "\n" + json.dumps(updateDoc) + "\n"
+
+    def getTargetURL(wikihost):
+        """
+        Get ES URL from wiki hostname
+        """
+        if wikihost in broardcastMap.value:
+            wiki = broardcastMap.value[wikihost]
+            return TARGET + "/" + wiki + "_content" + "/page/_bulk"
+        return None
+
+    def sendDataToES(data, url):
+        """
+        Send data to ES server
+        """
+        if len(data) < 1:
+            return
+        try:
+            r = requests.put(url, data=data)
+        except requests.exceptions.RequestException, e:
+            errorCounter.add(1)
+            logging.error("Failed to send update: " + str(e))
+            return False
+        if r.status_code != 200:
+            errorCounter.add(1)
+            r.close()
+            return False
+        parseResponse(r)
+        updateCounter.add(1)
+        return True
+
+    def parseResponse(resp):
+        respData = resp.json()
+        for item in respData.get('items', default={}):
+            if 'update' not in item:
+                continue
+            if 'status' not in item['update']:
+                continue
+            if item['update']['status'] != 200:
+                if 'error' in item['update'] and item['update']['error'][0:24] == 'DocumentMissingException':
+                    continue
+                failedDocumentCounter.add(1)
+
+    def sendDocumentsToES(documents):
+        """
+        Send a set of documents to ES
+        """
+        if len(documents) < 1:
+            return
+        url = getTargetURL(documents[0].project)
+        if not url:
+            return
+        data = ""
+        for document in documents:
+            data += documentData(document)
+        documentCounter.add(len(documents))
+        if not sendDataToES(data, url):
+            failedDocumentCounter.add(len(documents))
+
+    def addToList(listNow, element):
+        """
+        Add element to list, will send the data out once batch is full
+        """
+        if len(listNow) < ITEMS_PER_BATCH:
+            return listNow + [element]
+        # Treshold reached, send the list out
+        sendDocumentsToES(listNow)
+        return [element]
+
+    def mergeLists(listOne, listTwo):
+        """
+        Merge two lists, will send the data out once batch is full
+        """
+        newList = listOne + listTwo
+        if len(newList) < ITEMS_PER_BATCH:
+            return newList
+        sendDocumentsToES(newList)
+        return []
+
+    def sendCombined(data):
+        """
+        Send data for the key (hostname) to ES
+        """
+        sendDocumentsToES(data[1])
+
+    data = sqlContext.load(SOURCE)
+    # print "Count: %d\n" % data.count()
+    # Here's what is going on here: we combine the data by project,
+    # and when the list of projects reaches ITEMS_PER_BATCH we send them out
+    # The foreach() part will send out the ones that remained
+    data.map(lambda x: (x.project, x)).combineByKey(lambda x: [x], addToList, mergeLists).foreach(sendCombined)
+    print "%d documents processed, %d failed." % (documentCounter.value, failedDocumentCounter.value,)
+    print "%d requests successful, %d requests failed." % (updateCounter.value, errorCounter.value)
