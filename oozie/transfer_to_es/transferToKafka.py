fec00bc60b4497eb830b315964c1f1921184f81e
Erik Bernhardson <ebernhardson@wikimedia.org>
2018-08-02 13:54:51 -0700

    transfer_to_es kafka tweaks
    Talked it over and decided `cirrussearch.page-index-update` is
    a better topic name, with the gola of being descriptive of what
    the data inside the topic represents.
    
    Also It's probably not a big deal, but if we are going to pull the job
    down to a single executor we don't really want to force a giant sort.
    Rather than sort take the list of projects and produce to them one at
    a time. This also ensures if we allow producer parallelizm they will
    be producing records for the same wiki.
    
    Since the message volume here is high (benchmarked at ~12k messages/s
    per producer vs mean of 2.5k/s for the eqiad-main cluster) very basic
    rate limiting was added. The initial value is set to 1400/s which will
    spread the data out over ~24 hours.
    
    Bug: T200215
    Change-Id: I2066c67905a1e679593d0e25ae6ed2a5e071914e

--- oozie/transfer_to_es/transferToKafka.py
+++ oozie/transfer_to_es/transferToKafka.py
@@ -7,11 +7,12 @@ except ImportError:
     import findspark
     findspark.init()
     from pyspark import SparkContext
-from pyspark.sql import functions as F, SQLContext
+from pyspark.sql import SQLContext
 from optparse import OptionParser
 import json
 import kafka
 import subprocess
+import time
 
 oparser = OptionParser()
 oparser.add_option("-s", "--source", dest="source", help="source for the data", metavar="SOURCE")
@@ -20,11 +21,9 @@ oparser.add_option("-k", "--kafka-brokers", dest="brokers", metavar="URL", defau
 oparser.add_option('-t', '--topic', dest='topic', help="Kafka topic to produce into", metavar='TOPIC')
 oparser.add_option("-m", "--hostmap", dest="hostmap", help="Hostnames map in JSON",
     metavar="FILE", default="hostmap.json")
-oparser.add_option("-n", "--noop-within", dest="noop",
-    help="Only perform update if value has changed by more than this percentage",
-    metavar="NOOP_WITHIN")
 oparser.add_option("-f", "--field-name", dest="field", help="Name of elasticsearch field to populate",
     metavar="FIELD", default="score")
+oparser.add_option("-r", "--rate-limit", dest="rate", help="Maximum messages/s per producer")
 
 (options, args) = oparser.parse_args()
 
@@ -33,12 +32,28 @@ BROKERS = options.brokers.split(',')
 TOPIC = options.topic
 if not TOPIC:
     raise RuntimeError("...")
-NOOP_WITHIN = options.noop
 FIELD = options.field
+RATE = int(options.rate)
 if options.hostmap[0:24] == 'hdfs://analytics-hadoop/':
     hostMap = json.loads(subprocess.check_output(["hdfs", "dfs", "-cat", options.hostmap[23:]]))
 else:
     hostMap = json.load(open(options.hostmap))
+
+
+def ratelimit(rows):
+    now = time.monotonic if hasattr(time, 'monotonic') else time.time
+    next_reset = now() + 1
+    num_rows = 0
+    for row in rows:
+        time_remaining = next_reset - now()
+        if time_remaining < 0 or num_rows >= RATE:
+            if time_remaining > 0:
+                time.sleep(time_remaining)
+            num_rows = 0
+            next_reset = now() + 1
+        num_rows += 1
+        yield row
+
 
 print("Transferring from %s to %s" % (SOURCE, BROKERS[0]))
 
@@ -47,8 +62,6 @@ if __name__ == "__main__":
     sqlContext = SQLContext(sc)
     broadcastMap = sc.broadcast(hostMap)
     documentCounter = sc.accumulator(0)
-    updateCounter = sc.accumulator(0)
-    errorCounter = sc.accumulator(0)
     failedDocumentCounter = sc.accumulator(0)
 
     def getTargetWiki(wikihost):
@@ -86,18 +99,28 @@ if __name__ == "__main__":
         Pipe out the data to kafka
         """
         producer = kafka.KafkaProducer(bootstrap_servers=BROKERS,
+                                       client_id='transfer-to-cirrussearch',
                                        compression_type='gzip',
                                        api_version=(0, 9))
         try:
-            for row in rows:
+            for row in ratelimit(rows):
                 producer.send(TOPIC, row.encode('utf-8'))
                 documentCounter.add(1)
         finally:
             producer.close()
 
-    sqlContext.read.json(SOURCE) \
-        .sort(F.col('project')) \
-        .rdd.flatMap(documentData) \
-        .foreachPartition(send)
+    # Ideally we want to produce each wiki one after the other so when the data
+    # is consumed it creates segments in a few indices at a time instead of
+    # hitting all indices in a random order. Sorting the data by project
+    # requires deserializing the whole thing into memory at once, so instead
+    # work through the list one project at a time. parqet page stats will ensure
+    # we don't load most of the data for the hundreds of tiny projects.
+    df = sqlContext.read.parquet(SOURCE)
+    all_projects = [r.project for r in df.select(df['project']).drop_duplicates().collect()]
+    for project in all_projects:
+        print("Processing %s" % (project))
+        df.where(df['project'] == project) \
+            .rdd.flatMap(documentData) \
+            .foreachPartition(send)
 
-    print("%d documents processed, %d failed." % (documentCounter.value, failedDocumentCounter.value,))
+    print("%d documents processed, %d failed." % (documentCounter.value, failedDocumentCounter.value))
