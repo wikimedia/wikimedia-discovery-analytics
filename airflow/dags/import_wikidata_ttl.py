ae6dd8dac32c0bed805390985b498613af08af3e
David Causse <dcausse@wikimedia.org>
2020-08-25 18:03:07 +0200

    wikidata ttl import: force start and end dates for testing
    Change-Id: Ia23850832a191a7b49afda19f2c84e34e50901c4

--- airflow/dags/import_wikidata_ttl.py
+++ airflow/dags/import_wikidata_ttl.py
@@ -5,10 +5,9 @@
 - The refinery job runs hdfs_rsync every days around 1am and 3am
   (see puppet import_wikidata_entities_dumps.pp)
 """
-from datetime import timedelta
+from datetime import timedelta, datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.utils.dates import days_ago
 
 from wmf_airflow.hdfs_cli import HdfsCliSensor
 from wmf_airflow.spark_submit import SparkSubmitOperator
@@ -35,7 +34,8 @@ RDF_DATA_TABLE = dag_conf("rdf_data_table")
 default_args = {
     'owner': 'discovery-analytics',
     'depends_on_past': False,
-    'start_date': days_ago(1),
+    'start_date': datetime(2020, 7, 19),
+    'end_date': datetime(2020, 7, 30),
     'email': ['discovery-alerts@lists.wikimedia.org'],
     'email_on_failure': True,
     'email_on_retry': True,
