bc2f7f11ccd892b6538b318ddfc2b2690184c6a7
David Causse <dcausse@wikimedia.org>
2020-08-25 18:59:21 +0200

    Proper * expansion for variable imports
    Change-Id: I0b8963d900ca003ce43b5de0cf37f1b40f53c947

--- scap/scripts/import_airflow_variables.sh
+++ scap/scripts/import_airflow_variables.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-for config_path in "${SCAP_REV_DIR}/airflow/config/*.json"; do
+for config_path in "${SCAP_REV_DIR}/airflow/config/"*.json; do
     sudo -u analytics-search /usr/local/bin/airflow variables --import "${config_path}"
 done
