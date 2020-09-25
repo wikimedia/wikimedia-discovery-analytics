d1a619fdfb848635d7fcfc3c23c3847d573943b7
Erik Bernhardson <ebernhardson@wikimedia.org>
2020-09-25 15:01:47 -0700

    Add more debugging info to airflow_variables scap check
    Previous round of logging was insufficient. Expand to
    include more runtime information in the logs.
    
    Change-Id: Ibf174ec5bdaea0ded692d91672f0bb271003d5e6

--- scap/scripts/import_airflow_variables.sh
+++ scap/scripts/import_airflow_variables.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 echo "Importing variables to airflow"
-env
+AIRFLOW_CONFIG="${SCAP_REV_DIR}/airflow/config"
+echo "Sourcing files from $AIRFLOW_CONFIG"
+ls $AIRFLOW_CONFIG/*.json
 
-for config_path in "${SCAP_REV_DIR}/airflow/config/"*.json; do
+for config_path in ${AIRFLOW_CONFIG}/*.json; do
+    echo "Importing ${config_path}"
     sudo -u analytics-search /usr/local/bin/airflow variables --import "${config_path}"
 done
