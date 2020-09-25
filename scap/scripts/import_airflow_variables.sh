a13538880d5f2be538760ac26c672482351c9bf5
Erik Bernhardson <ebernhardson@wikimedia.org>
2020-09-25 15:21:59 -0700

    Correct path to airflow config in variables import
    This was referencing SCAP_REV_DIR, but the variable is SCAP_REV_PATH.
    
    Change-Id: I5570cc5d3b5247c0a0bf7a3d2c46c6646de098bb

--- scap/scripts/import_airflow_variables.sh
+++ scap/scripts/import_airflow_variables.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 echo "Importing variables to airflow"
-AIRFLOW_CONFIG="${SCAP_REV_DIR}/airflow/config"
+env
+AIRFLOW_CONFIG="${SCAP_REV_PATH}/airflow/config"
 echo "Sourcing files from $AIRFLOW_CONFIG"
-ls $AIRFLOW_CONFIG/*.json
+ls "$AIRFLOW_CONFIG"/*.json
 
-for config_path in ${AIRFLOW_CONFIG}/*.json; do
+for config_path in "${AIRFLOW_CONFIG}"/*.json; do
     echo "Importing ${config_path}"
     sudo -u analytics-search /usr/local/bin/airflow variables --import "${config_path}"
 done
