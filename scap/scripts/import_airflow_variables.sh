d999f76bfa4a85a612f88115bd17039c4d16fe22
Erik Bernhardson <ebernhardson@wikimedia.org>
2020-09-25 14:06:00 -0700

    Add debugging info to import_airflow_variables.sh
    The latest deployment included new variables, and scap reported
    running the check, but no variables were imported. Manually
    invoking the import script worked as expected. Add some extra
    information to the scap deploy output to help track down what
    is happening.
    
    Change-Id: Idb4684e7d05910f89146d032d9a80915527e86ca

--- scap/scripts/import_airflow_variables.sh
+++ scap/scripts/import_airflow_variables.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Importing variables to airflow"
+env
+
 for config_path in "${SCAP_REV_DIR}/airflow/config/"*.json; do
     sudo -u analytics-search /usr/local/bin/airflow variables --import "${config_path}"
 done
